663fa4168b819ae967a55ad41b32e5f32615c1f5
Benoît Barberousse <benoit@skillsesh.com>
2015-04-07 22:41:36 -0700

    Add actions to cli

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@ require("babel/register");
 
 var REPL = require('repl'),
     stores = require('./app/stores'),
+    actions = require('./app/actions'),
     dispatcher = require('./app/dispatcher'),
     data = require('./app/data');
 
@@ -10,3 +11,4 @@ var ReplServer = REPL.start({prompt: '$ '});
 ReplServer.context.stores = stores;
 ReplServer.context.dispatcher = dispatcher;
 ReplServer.context.data = data;
+ReplServer.context.actions = actions;
